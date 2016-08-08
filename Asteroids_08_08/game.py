3493b6e3c3dde54164aeb47086239610f6fd1764
Loic0405 <loic.michel@outlook.com>
2016-08-08 10:17:51 -0400

    Add files via upload
    08_08 :
    - Fixed an issue with the speed of the ship : now it can decrease when the user release a key.

---
+++ Asteroids_08_08/game.py
@@ -0,0 +1,19 @@
+import sys
+import pygame
+import pygame.locals
+from random import randrange as random_randrange
+import func
+import const
+
+def play():
+    
+    #Objects
+    asteroid_list = []
+    battleShip = obj.Ship((const.WINDOW_WIDTH // 2 - 33, const.WINDOW_HEIGHT // 2 - 44))
+
+
+    
+    
+    
+    
+    
